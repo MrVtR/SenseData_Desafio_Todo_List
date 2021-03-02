e166beeb8dee4440fabdaab5d1d8c7e665144e56
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-03-02 18:56:53 -0300

    Ajustado css do Header

--- src/components/NucleoTarefas/nucleoTarefas.jsx
+++ src/components/NucleoTarefas/nucleoTarefas.jsx
@@ -4,10 +4,16 @@ function Menu() {
   return (
     <>
       <div className="nucleo">
-        <header>
+        <h1 className="welcome">Bem-Vindo de volta, Vítor</h1>
+        <header className="header">
+          <div className="notificacao">
+            <img
+              src="https://img.icons8.com/ios/30/000000/bell.png"
+              alt="Notificação"
+            />
+          </div>
           <img src={profile} alt="Imagem Perfil" className="profile"></img>
         </header>
-        <h1 className="X">Bem-Vindo de volta, Vítor</h1>
       </div>
     </>
   );
