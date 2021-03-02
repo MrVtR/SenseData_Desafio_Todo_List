133d761f42dc9cc7a17e36bf6d8499a5619fe4ce
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-03-02 20:41:46 -0300

    Criado componente de Container

---
+++ src/components/ContainerTarefa/containerTarefa.jsx
@@ -0,0 +1,5 @@
+import './containerTarefa.scss';
+function ContainerTarefa() {
+  return <h1>GG</h1>;
+}
+export default ContainerTarefa;
--- src/services/export.js
+++ src/services/export.js
@@ -2,5 +2,6 @@ import ContadorTarefas from '../components/ContadorTarefas/contadorTarefas';
 import Menu from '../components/Menu/menu';
 import MenuTarefas from '../components/MenuTarefas/menuTarefas';
 import NucleoTarefas from '../components/NucleoTarefas/nucleoTarefas';
+import ContainerTarefa from '../components/ContainerTarefa/containerTarefa';
 
-export { ContadorTarefas, Menu, MenuTarefas, NucleoTarefas };
+export { ContadorTarefas, Menu, MenuTarefas, NucleoTarefas, ContainerTarefa };
