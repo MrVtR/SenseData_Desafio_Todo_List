de5104581857af0686f32612ac9af988e51a422a
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-03-02 19:49:06 -0300

    Icones e css ajustado no menu lateral

--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -1,5 +1,26 @@
 import './menu.scss';
 function Menu() {
-  return <div className="menuBar"></div>;
+  return (
+    <div className="menuBar">
+      <header className="header">
+        <img
+          src="https://img.icons8.com/dotty/64/000000/notepad.png"
+          alt="TitleIcon"
+        />
+      </header>
+      <img
+        src="https://img.icons8.com/wired/64/000000/plus.png"
+        className="img"
+        id="add"
+        alt="Adicionar"
+      />
+      <img
+        src="https://img.icons8.com/plasticine/100/000000/close-window.png"
+        className="img"
+        id="sair"
+        alt="Sair"
+      />
+    </div>
+  );
 }
 export default Menu;
--- src/components/MenuTarefas/menuTarefas.jsx
+++ src/components/MenuTarefas/menuTarefas.jsx
@@ -4,14 +4,18 @@ function menuTarefas() {
   return (
     <div className="menuTarefas">
       <h1 className="Header">Tarefas</h1>
-      <ContadorTarefas name="All Tasks" background="red" cont="10" />
-      <ContadorTarefas name="All Tasks" background="red" cont="10" />
-      <ContadorTarefas name="All Tasks" background="red" cont="10" />
-      <ContadorTarefas name="All Tasks" background="red" cont="10" />
-      <ContadorTarefas name="All Tasks" background="red" cont="10" />
-      <ContadorTarefas name="All Tasks" background="red" cont="10" />
-      <ContadorTarefas name="All Tasks" background="red" cont="10" />
-      <ContadorTarefas name="All Tasks" background="red" cont="10" />
+      <ContadorTarefas name="Todas as Tarefas" background="#172b4d" cont="10" />
+      <ContadorTarefas name="Gerência" background="#1665d8" cont="10" />
+      <ContadorTarefas name="Vendas" background="#f6c056" cont="10" />
+      <ContadorTarefas name="Operações" background="#f85252" cont="10" />
+      <ContadorTarefas name="Marketing" background="#6f48e1" cont="10" />
+      <ContadorTarefas name="Recursos Humanos" background="#f19445" cont="10" />
+      <ContadorTarefas name="Finanças" background="#60c354" cont="10" />
+      <ContadorTarefas
+        name="Serviço ao Consumidor"
+        background="#aa44f6"
+        cont="10"
+      />
     </div>
   );
 }
