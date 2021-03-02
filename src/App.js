cfd7e89b1e305b386ebf6960960c962b22b64820
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-03-02 15:26:12 -0300

    Adicionado primeiros arquivos e Route da página configurado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import Home from './pages/Home/index';
+import AddTask from './pages/AddTask/index';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/add" component={AddTask} />
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,6 @@
+function Home() {
+  console.log('Home');
+  return <h1>Home</h1>;
+}
+
+export default Home;
